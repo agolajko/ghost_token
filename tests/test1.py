bcae8b86071e7c818649cf8677bbd092d65e71dd
agolajko <agoston.lajko+github@gmail.com>
2022-05-09 16:12:40 +0100

    testing more

--- tests/test1.py
+++ tests/test1.py
@@ -270,9 +270,9 @@ def hash_orientation(node_one: Node, node_two: Node):
 
     if node_one_position_parity:
         # if node_one is in odd position
-        return mp_hash_named_tuple(node_two_position_parity, node_one_position_parity)
+        return mp_hash_named_tuple(node_two, node_one)
     else:
-        return mp_hash_named_tuple(node_one_position_parity, node_two_position_parity)
+        return mp_hash_named_tuple(node_one, node_two)
 
 
 def mp_hash_named_tuple(left_tuple: Node, right_tuple: Node):
@@ -285,21 +285,62 @@ def mp_hash_named_tuple(left_tuple: Node, right_tuple: Node):
 
     # get the height and position of the new hashed node
 
-    new_height = left_tuple.height + 1
+    new_height = left_tuple.height - 1
     new_position = int(right_tuple.position / 2)
 
     if left_zeros and right_zeros:
-        return((new_height, new_position, 0, 0, 0))
+        return(Node(new_height, new_position, 0, 0, 0))
     elif not left_zeros and right_zeros:
-        return((new_height, new_position, left_tuple[0]+1, left_tuple[1], left_tuple[2]))
+        return(Node(new_height, new_position, left_tuple.length+1, left_tuple.path, left_tuple.value))
     elif left_zeros and not right_zeros:
-        return((new_height, new_position, right_tuple[0]+1, right_tuple[1] + 2**right_tuple[0], right_tuple[2]))
+        return(Node(new_height, new_position, right_tuple.length+1, right_tuple.path + 2**right_tuple.length, right_tuple.value))
     else:
-        return((new_height, new_position, 0, 0, pedersen_hash(mp_inner(left_tuple[2:]), mp_inner(right_tuple[2:]))))
+        return(Node(new_height, new_position, 0, 0, pedersen_hash(mp_inner_named_tuple(left_tuple[2:]), mp_inner_named_tuple(right_tuple[2:]))))
 
 
-def mp_inner_tuple(inner_tuple):
+def mp_inner_named_tuple(inner_tuple):
     if inner_tuple[0] == 0:
         return(inner_tuple[2])
     else:
         return(pedersen_hash(inner_tuple[2], inner_tuple[1])+inner_tuple[0])
+
+
+def hash_branch_named_tuple(branch_list):
+    # takes a list of nodes represented as named tuples
+
+    # hash first two together and add their hash to new hashes list
+    # hash third element of branch list with first element of new_hashes
+    # hash fourth element of branch_list with second of new_hashes
+
+    # how will the hashing differ depending on the structure of the branch we are hashing
+
+    new_hashes = []
+
+    # hash first two together
+
+    first_hash = hash_orientation(branch_list[0], branch_list[1])
+
+    new_hashes.append(first_hash)
+
+    # loop over the two lists
+    # start hashing the last element of the
+
+    for branch_node in branch_list[2:]:
+        # joint_str = str(str(new_hashes[-1]) + str(node)).encode("ascii")
+        # node_hash = sha256(joint_str).hexdigest()
+
+        # how to load nodes into below hashing function so they are consistant with left, right policy
+        node_hash = hash_orientation(branch_node, new_hashes[-1])
+
+        new_hashes.append(node_hash)
+
+    return(new_hashes)
+
+
+test_branch_list = [Node(3, 7, 0, 0, 0), Node(
+    3, 6, 0, 0, 0), Node(2, 2, 1, 1, 1), Node(1, 0, 2, 2, 1)]
+
+test_branch_list2 = [Node(3, 4, 0, 0, 0), Node(
+    3, 5, 0, 0, 1), Node(2, 3, 0, 0, 0), Node(1, 0, 2, 2, 1)]
+
+hash_branch_named_tuple(test_branch_list2)
--- tests/test_branch_verify.py
+++ tests/test_branch_verify.py
@@ -9,10 +9,9 @@
 # verify contract gives same output as the python output
 
 
-
 import pytest
 import asyncio
-from typing import NamedTuple
+# from typing import NamedTuple
 
 from starkware.starknet.testing.contract import StarknetContract
 from starkware.starknet.testing.starknet import Starknet
@@ -20,23 +19,28 @@ from starkware.starknet.testing.starknet import Starknet
 
 @pytest.fixture
 async def starknet_ins() -> Starknet:
-	print("Hello")
-	return await Starknet.empty()
+    print("Hello")
+    return await Starknet.empty()
+
 
 @pytest.fixture
-async def branch_contract(starknet_ins : Starknet) -> StarknetContract:
-    return await starknet_ins.deploy(source="../contracts/cairo/branch_verify.cairo", cairo_path=["contracts"])
+async def branch_contract(starknet_ins: Starknet) -> StarknetContract:
+    # return await starknet_ins.deploy(source="/home/ago/projects/ghost_token/contracts/cairo/branch_verify.cairo", cairo_path=["contracts"])
+    return await starknet_ins.deploy(source="/home/ago/projects/ghost_token/contracts/cairo/branch_verify.cairo")
 
 
 @pytest.mark.asyncio
-async def test_branch(starknet_ins: Starknet, branch_contract : StarknetContract):
+async def test_branch(starknet_ins: Starknet, branch_contract: StarknetContract):
 
     # Invoke test
     print("does this run?")
-    res = await branch_contract.verify_branch().call()
+    leaf1 = (3, 5, 0, 0, 1)
+    branch1 = ((1, 0, 2, 2, 1))
+    total_len1 = 4
+    root_hash1 = 1428519069806529925910806899344787140252006517057140074734361523088172087989
+    res_hash = await branch_contract.verify_branch(leaf1, branch1, total_len1, root_hash1).call()
     print("does this run?")
-
-    assert res.res == 3 
-
+    print(res_hash.result.res)
+    # assert res.res == 3
 
     return()
