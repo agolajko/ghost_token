29c63429dc816f23a081c3f8b42028f417866242
agolajko <agoston.lajko+github@gmail.com>
2022-06-09 14:57:59 +0100

    some tx now show up in argent

--- src/addresses.tsx
+++ src/addresses.tsx
@@ -1,2 +1,3 @@
-export const starknetAddress = '0x071cdfdb934f450e8441dd201c5d89d5f6a82a68c02969de99f5481bd7a828f8';
+export const starknetAddress = '0x06c6baefe2e3948d4c823e1d1be041c7775b1fda8b6414b4caaf31fb9c3832ec';
+// export const starknetAddress = '0x071cdfdb934f450e8441dd201c5d89d5f6a82a68c02969de99f5481bd7a828f8';
 export const ethAddress = "0x15b01475bb3070912216dc393c3a782cc90fa1f7";
--- src/components/Transfer/index.tsx
+++ src/components/Transfer/index.tsx
@@ -4,6 +4,7 @@ import { useStarknetInvoke } from '@starknet-react/core'
 import { useStarknet } from '@starknet-react/core'
 import { useCurrentContract } from "../../lib/Contract";
 // import { useCurrentContract } from ''
+import { starknetAddress } from "../../addresses";
 
 
 // export function Transfer({ contract }: { contract?: Contract }) {
@@ -51,7 +52,7 @@ export function Transfer() {
     const send_data = {
       // "block": "230071",
       "block": block_number,
-      "contract": "0x071cdfdb934f450e8441dd201c5d89d5f6a82a68c02969de99f5481bd7a828f8",
+      "contract": starknetAddress,
       "variable": "variable"
     }
 
@@ -66,171 +67,58 @@ export function Transfer() {
         },
         body: JSON.stringify(send_data),
       })
-    // .then(response_data => response_data.json())
-    // .then(res => console.log(res))
-    // .then(merkleb_low_0_0 = response_data.)
 
-    // parse the result from the POST request to send it to the contract
-
-    // console.log(response_data.json())
 
     const response = await response_data.json()
     console.log(response)
     console.log("now")
     console.log(response.merkleb_high)
 
-    const state_root_ = state_root
-    // const state_root_ = String(BigInt(state_root))
-    const leaf_height = 251
-    const leaf_position = 778741677097751136629359488410499500026881115738990894538495916875030189956
+    // const state_root_ = state_root
+    const state_root_ = String(BigInt(state_root))
+    const leaf_height = response.merkleb_low[0][0]
+    const leaf_position = String(BigInt(response.merkleb_low[0][1]))
     const leaf_length = response.merkleb_low[0][2]
     const leaf_path = response.merkleb_low[0][3]
-    const leaf_value = response.merkleb_low[0][4]
-    console.log("response.merkleb_low.lenght")
-    console.log(response.merkleb_low.length)
-    // console.log(leaf_length)
+    const leaf_value = String(BigInt(response.merkleb_low[0][4]))
 
     console.log("response.merkleb_low")
     console.log(response.merkleb_low)
-    const branch_low = response.merkleb_low.slice(1, 5).flat();
 
-    console.log("merkleb_low_truncated.length")
-    const merkleb_low_truncated_length = branch_low.length
-    console.log(branch_low)
-
-    const total_low_len = String(251)
     const root_low_hash = String(BigInt("0x" + response.storage_root))
 
-    const branch_high = response.merkleb_high.slice(1, 5).flat();
-
-    const total_high_len = response.merkleb_high.length;
     const root_high_hash = String(BigInt("0x" + response.root_hash))
 
-    console.log("vars")
-    console.log(response.merkleb_low)
-    console.log(leaf_height)
-    console.log(leaf_position)
-    console.log(leaf_length)
-    console.log(leaf_path)
-    console.log(leaf_value)
-    console.log(branch_low)
-    console.log("branch_high")
-    console.log(branch_high)
-    console.log(root_low_hash)
-    console.log(root_high_hash)
-    console.log(state_root_)
-    console.log(branch_high)
 
-    console.log("type of vars")
-    console.log(typeof response.merkleb_low)
-    console.log(typeof leaf_height)
-    console.log(typeof leaf_position)
-    console.log(typeof leaf_length)
-    console.log(typeof leaf_path)
-    console.log(typeof leaf_value)
-    console.log(typeof branch_low)
-    console.log(typeof branch_high)
-    console.log(typeof root_low_hash)
-    console.log(typeof root_high_hash)
-    console.log(typeof state_root_)
-    console.log(typeof branch_high)
-    console.log(typeof total_high_len)
-    console.log(typeof total_low_len)
 
-    var branch_low_dict: { [id: string]: number } = {
-      "height": 1,
-      "position": 1,
-      "length": 1,
-      "path": 1,
-      "value": 1
-    };
-    console.log(typeof branch_low_dict)
 
-    var merkle_branch_low_dict = []
-    var merkle_branch_high_dict = []
-    for (let i = 1; i <= 8; i++) {
+    const merkle_branch_low_dict = []
+    const merkle_branch_high_dict = []
+
+
+    for (let i = 1; i <= 249; i++) {
       merkle_branch_low_dict.push({
-        "height": response.merkleb_low[i]["0"], "position": response.merkleb_low[i]["1"],
-        "length": response.merkleb_low[i]["2"], "path": response.merkleb_low[i]["3"], "value": response.merkleb_low[i]["4"]
+        height: String(BigInt(response.merkleb_low[i]["0"])), position: String(BigInt(response.merkleb_low[i]["1"])),
+        length: String(BigInt(response.merkleb_low[i]["2"])), path: String(BigInt(response.merkleb_low[i]["3"])), value: String(BigInt(response.merkleb_low[i]["4"]))
       })
     }
-    // for (let i = 0; i <= response.merkleb_high.length - 1; i++) {
-    for (let i = 0; i <= 8; i++) {
+    for (let i = 0; i <= response.merkleb_high.length - 1; i++) {
+      // for (let i = 0; i <= 8; i++) {
       merkle_branch_high_dict.push({
-        "height": response.merkleb_high[i]["0"], "position": response.merkleb_high[i]["1"],
-        "length": response.merkleb_high[i]["2"], "path": response.merkleb_high[i]["3"], "value": response.merkleb_high[i]["4"]
+        height: String(BigInt(response.merkleb_high[i]["0"])), position: String(BigInt(response.merkleb_high[i]["1"])),
+        length: String(BigInt(response.merkleb_high[i]["2"])), path: String(BigInt(response.merkleb_high[i]["3"])), value: String(BigInt(response.merkleb_high[i]["4"]))
       })
     }
 
-    // for (let i = 0; i <= 251; i++) {
-    //   merkle_branch_low_dict.push({
-    //     "height": response.merkleb_high[i], "position": response.merkleb_high[i],
-    //     "length": response.merkleb_high[i], "path": response.merkleb_high[i], "value": response.merkleb_high[i]
-    //   })
-    // }
+    const leaf = { height: leaf_height, position: String(BigInt(leaf_position)), length: leaf_length, path: leaf_path, value: String(BigInt(leaf_value)) }
 
-    console.log(response.merkleb_low.length)
-    console.log(merkle_branch_low_dict)
-    console.log(merkle_branch_high_dict)
-
-
-    // none of these function calls  work
     invoke({
-      args: [1, { height: 71, position: 72, length: 73, path: 74, value: 75 },
-        [{ height: 71, position: 71, length: 73, path: 74, value: 75 }], 8, 9, [{ height: 101, position: 102, length: 103, path: 104, value: 105 }], 11, 12
+      args: [state_root_, leaf,
+        merkle_branch_low_dict, root_low_hash, merkle_branch_high_dict, root_high_hash
       ], metadata: { method: "verify_increment" }
     })
 
-    // invoke({
-    //   args: [[1], { height: 71, position: 72, length: 73, path: 74, value: 75 }, [1],
-    //   [{ height: 71, position: 71, length: 73, path: 74, value: 75 }], [8], [9], [1], [{ height: 101, position: 102, length: 103, path: 104, value: 105 }], [11], [12]
-    //   ], metadata: { method: "verify_increment" }
-    // })
 
-    // invoke({
-    //   args: [1, { height: 71, position: 72, length: 73, path: 74, value: 75 },
-    //     [{ height: 71, position: 72, length: 73, path: 74, value: 75 }, { height: 71, position: 72, length: 73, path: 74, value: 75 }],
-    //     [8], 9, [{ height: 101, position: 102, length: 103, path: 104, value: 105 }, { height: 71, position: 72, length: 73, path: 74, value: 75 }], [11], 12
-    //   ], metadata: { method: "verify_increment" }
-    // })
-
-
-    //invoke({
-    //  args: [1, 2, 3, 4, 5, 6,
-    //    [{ "height": 71, "position": 72, "length": 73, "path": 74, "value": 75 }], 8, 9, [{ "height": 101, "position": 102, "length": 103, "path": 104, "value": 105 }], 11, 12
-    //  ], metadata: { method: "verify_increment" }
-    //})
-    //invoke({
-    //  args: [1, 2, 3, 4, 5, 6,
-    //    [{ "height": 71, "position": 72, "length": 73, "path": 74, "value": 75 }], 8, 9, [{ "height": 101, "position": 102, "length": 103, "path": 104, "value": 105 }], 11, 12
-    //  ], metadata: { method: "verify_increment" }
-    //})
-
-    // // this works
-    // invoke_init({
-    //   args: ["0x12", "1"], metadata: { method: "initialise" }
-    // })
-
-
-    // invoke_init({
-    //   args: [state_root_, leaf_height], metadata: { method: "initialise" }
-    // })
-
-
-
-    // invoke({
-    //   args: [state_root_, leaf_height, leaf_position, leaf_length, leaf_path, leaf_value,
-    //     [merkle_branch_low_dict], total_low_len, root_low_hash, [merkle_branch_high_dict], total_high_len, root_high_hash
-    //   ], metadata: { method: "verify_increment" }
-    // })
-    // invoke({
-    //   args: [state_root_, leaf_height, leaf_position, leaf_length, leaf_path, leaf_value,
-    //     branch_low, total_low_len, root_low_hash, branch_high, total_high_len, root_high_hash
-    //     // args: [state_root_: state_root_, leaf: { leaf_height, leaf_position, leaf_length, leaf_path, leaf_value },
-    //     // branch_low: branch_low, total_low_len: total_low_len, root_low_hash: root_low_hash, branch_high: branch_high, total_high_len: total_high_len, root_high_hash: root_high_hash
-    //   ], metadata: { method: "verify_increment" }
-    // })
-    // return ({state_root_, leaf_height, leaf_position, leaf_length, leaf_path, leaf_value, branch_low, total_low_len, root_low_hash, branch_high, total_high_len, root_high_hash})
     console.log("get ehre?")
   }
 
